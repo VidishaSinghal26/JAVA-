6a42360064308cd71765f1b6b50ddfbb3a2a6447
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2021-11-10 18:23:38 +0530

    Add files via upload

---
+++ src/WapperClass.java
@@ -0,0 +1,18 @@
+
+public class WapperClass {
+    public static void main(String[] args) {
+        System.out.println("Integer range:");
+        System.out.println("min:-"+Integer.MIN_VALUE);
+        System.out.println("max:-"+Integer.MAX_VALUE);
+        System.out.println("Double range:");
+        System.out.println("min:-"+Double.MIN_VALUE);
+        System.out.println("max:-"+Double.MAX_VALUE);
+        System.out.println("Long range:");
+        System.out.println("min:-"+Long.MIN_VALUE);
+        System.out.println("max:-"+Long.MAX_VALUE);
+        System.out.println("Short range:");
+        System.out.println("min:-"+Short.MIN_VALUE);
+        System.out.println("max:-"+Short.MAX_VALUE);
+    }
+
+}
