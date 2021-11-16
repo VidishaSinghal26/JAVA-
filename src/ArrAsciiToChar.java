3c904b7d33e3d8199d8d1d4b6de8c587c3520a51
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2021-11-16 17:44:35 +0530

    Add files via upload

---
+++ src/ArrAsciiToChar.java
@@ -0,0 +1,9 @@
+public class ArrAsciiToChar {
+    public static void main(String[] args) {
+        int arr[] ={97,54,99,67};
+        for (int i =0;i< arr.length;i++){
+            System.out.print((char)arr[i]+" ");
+        }
+    }
+}
+
