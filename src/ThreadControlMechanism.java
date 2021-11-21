afccafef66862a879466db8ff288e29f1aba71c7
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2021-11-21 16:13:42 +0530

    Update and rename ThreadPriority.java to ThreadControlMechanism.java

--- src/ThreadControlMechanism.java
+++ src/ThreadControlMechanism.java
@@ -1,23 +1,21 @@
-class MyThreadPriority implements Runnable {
+class MyThreadControlMechanism implements Runnable {
     public void run() {
         for (int i = 1; i <= 10; i++) {
-            Thread.currentThread().setPriority(i);
             if (i == 6) {
                 try {
                     Thread.currentThread().sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
             }
-            System.out.println(Thread.currentThread().getPriority());
+            System.out.println(i);
         }
     }
 }
-class ThreadPriority
+class ThreadControlMechanism
 {
     public static void main(String[] args) {
-        MyThreadPriority t1 = new MyThreadPriority();
+        MyThreadControlMechanism t1 = new MyThreadControlMechanism();
         Thread obj=new Thread(t1);
         obj.start();
     }
