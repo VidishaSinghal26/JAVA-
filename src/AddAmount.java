3d8e82f82a8b2c050e6e67728996e10755d00c25
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2021-12-03 00:20:58 +0530

    Create AddAmount.java

---
+++ src/AddAmount.java
@@ -0,0 +1,30 @@
+class AddAmount
+{
+    private int amount = 50;
+
+    public AddAmount()
+    {
+
+    }
+
+    public AddAmount(int a)
+    {
+        amount = a+amount;
+    }
+
+    public void displayAmount()
+    {
+        System.out.println(amount);
+    }
+}
+
+class Ans
+{
+    public static void main(String[] args)
+    {
+        AddAmount a = new AddAmount();
+        AddAmount b = new AddAmount(40);
+        a.displayAmount();
+        b.displayAmount();
+    }
+}
