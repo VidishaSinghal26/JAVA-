d2b08eb69d30bddb8c98a28f8ac8d2698e58d4e6
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2022-01-06 23:06:07 +0530

    Update Codechef.java

--- src/Codechef.java
+++ src/Codechef.java
@@ -1,3 +1,6 @@
+/*You are given a string containing characters A and B only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are 
+allowed to delete zero or more characters in the string.*/
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
