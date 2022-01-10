21fb79447dbd6e22f26cb599e924d0cc7d698be8
VidishaSinghal26 <90644367+VidishaSinghal26@users.noreply.github.com>
2022-01-10 23:38:40 +0530

    Get position of duplicate elements in array

---
+++ src/Duplicate.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Duplicate {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int arr[] = new int[n];
+        for (int i=0;i<n;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        for (int i=0;i<n;i++)
+        {
+            for (int j=i+1;j <n;j++)
+            {
+                if(arr[i]==arr[j])
+                {
+                    System.out.println(j+" position");
+                }
+            }
+        }
+    }
+}
